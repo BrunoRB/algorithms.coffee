cc810554dfd6bb16706ab2929a235da7d8c09872
Bruno Roberto Búrigo <brunorb8@hotmail.com>
2014-07-05 15:56:45 -0300

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
                     './dist/algorithms.coffee.min.js': './dist/algorithms.coffee.js'
                 },
                 options: {
-                    banner: license
+                    banner: licenseHeader + '**/\n'
                 }
             }
         }
@@ -82,22 +82,24 @@ module.exports = function(grunt) {
 
 };
 
-var license = '/** \
+var licenseHeader = '/**\
     The MIT License (MIT)\n\
     Copyright (c) 2014 Bruno Roberto Búrigo\n\
-    https://github.com/BrunoRB/algorithms.coffee\n\
-    Permission is hereby granted, free of charge, to any person obtaining a copy \
-    of this software and associated documentation files (the "Software"), to deal \
-    in the Software without restriction, including without limitation the rights \
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell \
-    copies of the Software, and to permit persons to whom the Software is \
-    furnished to do so, subject to the following conditions: \
-    The above copyright notice and this permission notice shall be included in all \
-    copies or substantial portions of the Software. \
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, \
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE \
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER \
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, \
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE \
-    SOFTWARE.*/\n';
+    https://github.com/BrunoRB/algorithms.coffee';
+
+var license = licenseHeader + '\n\n\
+    Permission is hereby granted, free of charge, to any person obtaining a copy\n\
+    of this software and associated documentation files (the "Software"), to deal\n\
+    in the Software without restriction, including without limitation the rights\n\
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n\
+    copies of the Software, and to permit persons to whom the Software is\n\
+    furnished to do so, subject to the following conditions:\n\
+    The above copyright notice and this permission notice shall be included in all\n\
+    copies or substantial portions of the Software.\n\
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n\
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n\
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n\
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n\
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n\
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n\
+    SOFTWARE.\n**/\n';
